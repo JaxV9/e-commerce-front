b8b77bfc40f596796517b93c150e5814da1ef70a
JaxV9 <layanj.pro@gmail.com>
2025-07-23 11:24:32 +0200

    feat: pages added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProductDetailsComponent } from './pages/products/product-details/product-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'products',
+    component: HomeComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: ':id',
+        component: ProductDetailsComponent,
+      },
+    ],
+  },
+  {
+    path: '',
+    component: HomeComponent,
+  },
+];
